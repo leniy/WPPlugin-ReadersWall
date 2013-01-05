8cc36b489091c353a2146c212a6886633e53ebfb
Leniy Tsan <m@leniy.org>
2013-01-05 08:09:48 +0000

    Big version update
    git-svn-id: http://plugins.svn.wordpress.org/readers-wall/trunk@648233 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readers-wall.php
+++ readers-wall.php
@@ -3,7 +3,7 @@
 	Plugin Name:Readers Wall
 	Plugin URI: http://blog.leniy.info/readers-wall.html
 	Description: 高度自定制性能的读者墙
-	Version: 0.1
+	Version: 1.1.5
 	Author: leniy
 	Author URI: http://blog.leniy.info/
 */
@@ -25,105 +25,40 @@
 	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+$thisplugin_author = "Leniy";
+$thisplugin_url = "http://blog.leniy.info/readers-wall.html";
+$thisplugin_name = "Readers Wall";
+$thisplugin_version = "1.1.5";
+
 register_activation_hook(__FILE__, 'qw_RW_act');
 register_deactivation_hook(__FILE__, 'qw_RW_deact');
 
 function qw_RW_setdefault() {
-	$default_css = "
-.RW-btn {
-    padding-left:50px;
-    height:50px;
-    display:inline-block;
-    position:relative;
-    border:1px solid #80ab5d;
-    -webkit-box-shadow:0px 1px 1px rgba(255,255,255,0.8) inset, 1px 1px 3px rgba(0,0,0,0.2);
-    -moz-box-shadow:0px 1px 1px rgba(255,255,255,0.8) inset, 1px 1px 3px rgba(0,0,0,0.2);
-    box-shadow:0px 1px 1px rgba(255,255,255,0.8) inset, 1px 1px 3px rgba(0,0,0,0.2);
-    -webkit-border-radius:4px;
-    -moz-border-radius:4px;
-    border-radius:4px;
-    clear:both;
-    overflow:hidden;
-    -webkit-transition:box-shadow 0.3s ease-in-out;
-    -moz-transition:box-shadow 0.3s ease-in-out;
-    -o-transition:box-shadow 0.3s ease-in-out;
-    transition:box-shadow 0.3s ease-in-out;
-}
-.RW-btn img{
-    position:absolute;
-    left:0px;
-    top:0px;
-    width:100%; 
-    height:100%;
-    max-width:100%;
-    max-height:100%;
-    border:none;
-    -webkit-transition:all 0.3s ease-in-out;
-    -moz-transition:all 0.3s ease-in-out;
-    -o-transition:all 0.3s ease-in-out;
-    transition:all 0.3s ease-in-out;
-}
-.RW-btn .RW-btn-slide-text{
-    position:absolute;
-    font-size:20px;
-    top:10px;
-    left:10px;
-    color:#6d954e;
-    opacity:0;
-    text-shadow:0px 1px 1px rgba(255,255,255,0.4);
-    -webkit-transition:opacity 0.2s ease-in-out;
-    -moz-transition:opacity 0.2s ease-in-out;
-    -o-transition:opacity 0.2s ease-in-out;
-    transition:opacity 0.2s ease-in-out;
-}
-
-
-
-.RW-btn:hover{
-    -webkit-box-shadow:0px 1px 1px rgba(255,255,255,0.8) inset, 1px 1px 5px rgba(0,0,0,0.4);
-    -moz-box-shadow:0px 1px 1px rgba(255,255,255,0.8) inset, 1px 1px 5px rgba(0,0,0,0.4);
-    box-shadow:0px 1px 1px rgba(255,255,255,0.8) inset, 1px 1px 5px rgba(0,0,0,0.4);
-}
-.RW-btn:hover img{
-    -webkit-transform:scale(10);
-    -moz-transform:scale(10);
-    -ms-transform:scale(10);
-    -o-transform:scale(10);
-    transform:scale(10);
-    opacity:0;
-}
-.RW-btn:hover .RW-btn-slide-text{
-    opacity:1;
-}
-.RW-btn:active{
-    position:relative;
-    top:1px;
-    background:#80ab5d;
-    -webkit-box-shadow:1px 1px 2px rgba(0,0,0,0.4) inset;
-    -moz-box-shadow:1px 1px 2px rgba(0,0,0,0.4) inset;
-    box-shadow:1px 1px 2px rgba(0,0,0,0.4) inset;
-    border-color:#a9db80;
-}
-";
+	$default_css = "animated_with_css3.css";
 	$default_shortcode = "readerswall";
-	update_option("qw_RW_css", $default_css);//保存css样式
+	update_option("qw_RW_css", $default_css);//保存css样式文件名
 	update_option("qw_RW_shortcode", $default_shortcode);//保存文章调用时的短代码
 	update_option("qw_RW_shownumber", "64");//展示评论数排名前多少的用户
+	update_option("qw_RW_commentatleast", "1");//该用户至少发表评论多少条才会显示
 	update_option("qw_RW_days", "180");//统计多少天内的评论
 }
 
 function qw_RW_act() {
-	add_option("qw_RW_css", $default_css);//保存css样式
-	add_option("qw_RW_shortcode", $default_shortcode);//保存文章调用时的短代码
-	add_option("qw_RW_shownumber", "10");//展示评论数排名前多少的用户
-	add_option("qw_RW_days", "180");//统计多少天内的评论
+	add_option("qw_RW_css", "");//保存css样式文件名
+	add_option("qw_RW_manualcss", "");//保存自定义css样式内容
+	add_option("qw_RW_shortcode", "");//保存文章调用时的短代码
+	add_option("qw_RW_shownumber", "");//展示评论数排名前多少的用户
+	add_option("qw_RW_commentatleast", "");//该用户至少发表评论多少条才会显示
+	add_option("qw_RW_days", "");//统计多少天内的评论
 	qw_RW_setdefault();
 }
 
 function qw_RW_deact() {
 	delete_option("qw_RW_css");
+	delete_option("qw_RW_manualcss");
 	delete_option("qw_RW_shortcode");
 	delete_option("qw_RW_shownumber");
+	delete_option("qw_RW_commentatleast");
 	delete_option("qw_RW_days");
 }
 
@@ -144,12 +79,17 @@ function qw_RW_setpage() {
 		update_option("qw_RW_css", $_POST['qw_RW_css']);
 		update_option("qw_RW_shortcode", $_POST['qw_RW_shortcode']);
 		update_option("qw_RW_shownumber", $_POST['qw_RW_shownumber']);
+		update_option("qw_RW_commentatleast", $_POST['qw_RW_commentatleast']);
 		update_option("qw_RW_days", $_POST['qw_RW_days']);
 		echo "<div id=\"message\" class=\"rwupdate\"><p>更新保存成功，请在文章中插入[" . get_option("qw_RW_shortcode") . "]启用读者墙</p></div>";
 	}
 	if($_POST['qw_RW_resetbtn']=="恢复默认设置") {
 		qw_RW_setdefault();
-		echo "<div id=\"message\" class=\"rwupdate\"><p>恢复成功，请在文章中插入[" . get_option("qw_RW_shortcode") . "]启用读者墙</p></div>";
+		echo "<div id=\"message\" class=\"rwupdate\"><p>恢复成功，请在文章中插入[" . get_option("qw_RW_shortcode") . "]启用读者墙</p><p>自定义css样式不会清除；如欲重置，手动清空右下侧单元格您输入的内容即可</p></div>";
+	}
+	if($_POST['qw_RW_savebtn']=="保存") {
+		update_option("qw_RW_manualcss", $_POST['qw_RW_manualcss']);
+		echo "<div id=\"message\" class=\"rwupdate\"><p>自定义css文件保存成功，请再手动点击一次“更新按钮”，以确认启用</p></div>";
 	}
 
 	?>
@@ -168,6 +108,8 @@ function qw_RW_setpage() {
 	}
 	</style>
 
+<table border="1">
+<td>
 	<div><form method="post">
 		<table class="form-table">
 			<tbody>
@@ -186,6 +128,13 @@ function qw_RW_setpage() {
 					</td>
 				</tr>
 				<tr>
+					<th scope="row">最少评论数</th>
+					<td>
+						<input class="regular-text" name="qw_RW_commentatleast" type="text" id="qw_RW_commentatleast" value="<?php echo get_option("qw_RW_commentatleast"); ?>" />
+						<p>该用户至少发表评论多少条才会显示？</p>
+					</td>
+				</tr>
+				<tr>
 					<th scope="row">依照多少天内的评论进行排名</th>
 					<td>
 						<input class="regular-text" name="qw_RW_days" type="text" id="qw_RW_days" value="<?php echo get_option("qw_RW_days"); ?>" />天
@@ -195,8 +144,12 @@ function qw_RW_setpage() {
 				<tr>
 					<th scope="row">css样式</th>
 					<td>
-						<textarea name="qw_RW_css" rows="5" cols="50" id="qw_RW_css" class="large-text code"><?php echo get_option('qw_RW_css'); ?></textarea>
-						<p>请确保输入的css正确无误</p>
+						<input type="radio" id="animated_with_css3.css" name="qw_RW_css" value="animated_with_css3.css" checked="checked" onclick="qwhide()" />缩放动画<br>
+						<input type="radio" id="animated_shift.css" name="qw_RW_css" value="animated_shift.css" onclick="qwhide()" />偏移动画<br>
+						<input type="radio" id="myjquery.js" name="qw_RW_css" value="myjquery.js" onclick="qwhide()" disabled />jquery版本敬请期待<br>
+						<input type="radio" id="manualcss" name="qw_RW_css" value="manualcss" onclick="qwshow()"/>自定义css<br>
+						<textarea id="temptemptemp" style="display:none;"><?php echo get_option("qw_RW_css");?></textarea>
+						<p>如果选择自定义css，请确保输入的css正确无误</p>
 					</td>
 				</tr>
 			</tbody>
@@ -204,8 +157,30 @@ function qw_RW_setpage() {
 		<input type="submit" name="qw_RW_btn" value="更新" class="button-primary" />
 		<input type="submit" name="qw_RW_resetbtn" value="恢复默认设置" class="button-primary" />
 	</form></div>
-	<hr>
-	<iframe frameborder="0" src="http://blog.leniy.info/readers-wall.html" scrolling="auto" noresize="" width="100%" height="500px"></iframe>
+</td>
+<td id="manualarea" style="display:none;">
+	<form method="post">
+	自定义css文件编辑区
+	<textarea name="qw_RW_manualcss" rows="23" cols="50" id="qw_RW_manualcss" class="large-text code"><?php echo get_option('qw_RW_manualcss'); ?></textarea>
+	<input type="submit" name="qw_RW_savebtn" value="保存" class="button-primary" />
+	</form>
+</td>
+</table>
+						<SCRIPT LANGUAGE="JavaScript">
+							function qwshow(){
+								a=document.getElementById('manualarea').style.display="";
+							}
+							function qwhide(){
+								document.getElementById('manualarea').style.display="none";
+							}
+							b=document.getElementById('temptemptemp').value;
+							c=document.getElementById(b);
+							c.checked="TRUE";
+							c.click();
+						</script>
+
+	<div style="display:none;"><hr>
+	<iframe frameborder="0" src="http://blog.leniy.info/readers-wall.html" scrolling="auto" noresize="" width="100%" height="500px"></iframe></div>
 	<?php
 }
 
@@ -216,8 +191,10 @@ add_shortcode(get_option("qw_RW_shortcode"), "qw_RW_page");
 
 function qw_RW_page() {
 	global $wpdb;
+	$qw_RW_css = get_option('qw_RW_css');
 	$qw_RW_shownumber = get_option('qw_RW_shownumber');
 	$qw_RW_days = get_option('qw_RW_days');
+	$qw_RW_commentatleast = get_option('qw_RW_commentatleast');
 	$query = "
 	SELECT
 		COUNT( comment_author_email ) AS number,
@@ -240,27 +217,34 @@ function qw_RW_page() {
 	";
 	$RWs = $wpdb->get_results($query);
 	foreach ($RWs as $RW) {
-		$temp_number = $RW->number;
-		$temp_email = $RW->comment_author_email;
-		$temp_author = $RW->comment_author;
-		$temp_url = $RW->comment_author_url ? $RW->comment_author_url : "#";
-		$temp_avatar = get_avatar($RW, 50, plugins_url('readers-wall/default.png'));
+		if ($RW->number >= $qw_RW_commentatleast) {
+			$temp_number = $RW->number;
+			$temp_email = $RW->comment_author_email;
+			$temp_author = $RW->comment_author;
+			$temp_url = $RW->comment_author_url ? $RW->comment_author_url : "http://blog.leniy.info";
+			$temp_avatar = get_avatar($RW, 50, plugins_url('readers-wall/resource/default.png'));
 
-		$tmp = "
-<a title=\"" . $temp_author . "  (" . $temp_number . "条评论)\" href=\"" . $temp_url . "\" target=\"_blank\" class=\"RW-btn\" rel=\"nofollow\">
-" . "<span class=\"RW-btn-slide-text\">" . $temp_number . "</span>" . $temp_avatar . "</a>";
-		$output .= $tmp;
+			if ($qw_RW_css == "animated_with_css3.css") {
+				$output .= "<a title=\"" . $temp_author . "  (" . $temp_number . "条评论)\" href=\"" . $temp_url . "\" target=\"_blank\" class=\"RW-btn\" rel=\"nofollow\">" . "<span class=\"RW-btn-slide-text\">" . $temp_number . "</span>" . $temp_avatar . "</a>";
+			}
+			if ($qw_RW_css == "animated_shift.css") {
+				$output .= "<li><a target=\"_blank\" href=\"" . $temp_url . "\">" . $temp_avatar . "<em>" . $temp_author . "</em> <strong>" . $temp_number . "</strong></br></a></li>";
+
+			}
+			if ($qw_RW_css == "manualcss") {
+				$output .= "<a title=\"" . $temp_author . "  (" . $temp_number . "条评论)\" href=\"" . $temp_url . "\" target=\"_blank\" class=\"RW-btn\" rel=\"nofollow\">" . "<span class=\"RW-btn-slide-text\">" . $temp_number . "</span>" . $temp_avatar . "</a>";
+			}
+
+		}
 	}
-	$output = "<div class=\"readers-wall\">" . $output . "</div>";
-	$css = get_option('qw_RW_css');
-	return "<style type=\"text/css\">" . $css . "</style>" . $output;
+	$output = "<ul class=\"readers-wall\">" . $output . "</ul>";
+	if ($qw_RW_css == "manualcss") {
+		$css = '<style type="text/css">' . get_option("qw_RW_manualcss") . '</style>';
+	}
+	else {
+		$css = '<link type="text/css" rel="stylesheet" href="' . plugins_url('readers-wall/style/' . $qw_RW_css) . '" />';
+	}
+	return $css . $output;
 }
-
-/*
-	References:
-		1.Part of the css3 style from "Animated Buttons with CSS3"(Author:Anonymous)
-		2.Mysql code for count of the comments came from zhidao.baidu.com
-*/
-
 
 ?>
